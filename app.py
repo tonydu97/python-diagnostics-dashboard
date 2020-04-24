48419534229b14f852bfc05f6643235eee203f55
Du <TDu@crai.com>
2020-04-24 18:36:50 -0400

    template setup

--- app.py
+++ app.py
@@ -1,37 +1,358 @@
+import pathlib
+import os
+
+
 import dash
+import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
-
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-app.layout = html.Div(children=[
-    html.Header(children = [
-    html.H1(children='Charles River Associates')
-    ], style = {'background-color': 'red'}),
-    
-    html.H2('NextEra M&A - Project Bulldog'),
-
-    html.Div(children='''
-        Tony Du's python dashboard project
-    '''),
+import dash_bootstrap_components as dbc
 
 
+import pandas as pd
+import numpy as np
 
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+
+# global vars
+raw_results_dir = 'C:/Users/tdu/python/Raw results/'
+lst_baa = ['FG', 'DUK', 'ALGAMS']
+lst_utilities = ['NextEra Energy Inc', 'Southern Co']
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+server = app.server  # for Heroku deployment
+
+
+NAVBAR = dbc.Navbar(
+    children=[
+        html.A(
+            # Use row and col to control vertical alignment of logo / brand
+            dbc.Row(
+                [
+                    dbc.Col(html.Img(src=app.get_asset_url('cra-logo.png'), height="30px")),
+                    dbc.Col(
+                        dbc.NavbarBrand("Bank Customer Complaints", className="ml-2")
+                    ),
+                ],
+                align="center",
+                no_gutters=True,
+            ),
+            href="https://plot.ly",
+        )
+    ],
+    color="dark",
+    dark=True,
+    sticky="top",
+)
+
+LEFT_COLUMN = dbc.Jumbotron(
+    [
+        html.H4(children="Select bank & dataset size", className="display-5"),
+        html.Hr(className="my-2"),
+        html.Label("Select percentage of dataset", className="lead"),
+        html.P(
+            "(Lower is faster. Higher is more precise)",
+            style={"fontSize": 10, "font-weight": "lighter"},
+        ),
+        dcc.Slider(
+            id="n-selection-slider",
+            min=1,
+            max=100,
+            step=1,
+            marks={
+                0: "0%",
+                10: "",
+                20: "20%",
+                30: "",
+                40: "40%",
+                50: "",
+                60: "60%",
+                70: "",
+                80: "80%",
+                90: "",
+                100: "100%",
+            },
+            value=20,
+        ),
+        html.Label("Select a bank", style={"marginTop": 50}, className="lead"),
+        html.P(
+            "(You can use the dropdown or click the barchart on the right)",
+            style={"fontSize": 10, "font-weight": "lighter"},
+        ),
+        dcc.Dropdown(
+            id="bank-drop", clearable=False, style={"marginBottom": 50, "font-size": 12}
+        ),
+        html.Label("Select time frame", className="lead"),
+        html.Div(dcc.RangeSlider(id="time-window-slider"), style={"marginBottom": 50}),
+        html.P(
+            "(You can define the time frame down to month granularity)",
+            style={"fontSize": 10, "font-weight": "lighter"},
+        ),
+    ]
+)
+
+LDA_PLOT = dcc.Loading(
+    id="loading-lda-plot", children=[dcc.Graph(id="tsne-lda")], type="default"
+)
+LDA_TABLE = html.Div(
+    id="lda-table-block",
+    children=[
+        dcc.Loading(
+            id="loading-lda-table",
+            children=[
+                dash_table.DataTable(
+                    id="lda-table",
+                    style_cell_conditional=[
+                        {
+                            "if": {"column_id": "Text"},
+                            "textAlign": "left",
+                            "whiteSpace": "normal",
+                            "height": "auto",
+                            "min-width": "50%",
+                        }
+                    ],
+                    style_data_conditional=[
+                        {
+                            "if": {"row_index": "odd"},
+                            "backgroundColor": "rgb(243, 246, 251)",
+                        }
+                    ],
+                    style_cell={
+                        "padding": "16px",
+                        "whiteSpace": "normal",
+                        "height": "auto",
+                        "max-width": "0",
+                    },
+                    style_header={"backgroundColor": "white", "fontWeight": "bold"},
+                    style_data={"whiteSpace": "normal", "height": "auto"},
+                    filter_action="native",
+                    page_action="native",
+                    page_current=0,
+                    page_size=5,
+                    columns=[],
+                    data=[],
+                )
             ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
-        }
-    )
-])
+            type="default",
+        )
+    ],
+    style={"display": "none"},
+)
 
+LDA_PLOTS = [
+    dbc.CardHeader(html.H5("Topic modelling using LDA")),
+    dbc.Alert(
+        "Not enough data to render LDA plots, please adjust the filters",
+        id="no-data-alert-lda",
+        color="warning",
+        style={"display": "none"},
+    ),
+    dbc.CardBody(
+        [
+            html.P(
+                "Click on a complaint point in the scatter to explore that specific complaint",
+                className="mb-0",
+            ),
+            html.P(
+                "(not affected by sample size or time frame selection)",
+                style={"fontSize": 10, "font-weight": "lighter"},
+            ),
+            LDA_PLOT,
+            html.Hr(),
+            LDA_TABLE,
+        ]
+    ),
+]
+WORDCLOUD_PLOTS = [
+    dbc.CardHeader(html.H5("Most frequently used words in complaints")),
+    dbc.Alert(
+        "Not enough data to render these plots, please adjust the filters",
+        id="no-data-alert",
+        color="warning",
+        style={"display": "none"},
+    ),
+    dbc.CardBody(
+        [
+            dbc.Row(
+                [
+                    dbc.Col(
+                        dcc.Loading(
+                            id="loading-frequencies",
+                            children=[dcc.Graph(id="frequency_figure")],
+                            type="default",
+                        )
+                    ),
+                    dbc.Col(
+                        [
+                            dcc.Tabs(
+                                id="tabs",
+                                children=[
+                                    dcc.Tab(
+                                        label="Treemap",
+                                        children=[
+                                            dcc.Loading(
+                                                id="loading-treemap",
+                                                children=[dcc.Graph(id="bank-treemap")],
+                                                type="default",
+                                            )
+                                        ],
+                                    ),
+                                    dcc.Tab(
+                                        label="Wordcloud",
+                                        children=[
+                                            dcc.Loading(
+                                                id="loading-wordcloud",
+                                                children=[
+                                                    dcc.Graph(id="bank-wordcloud")
+                                                ],
+                                                type="default",
+                                            )
+                                        ],
+                                    ),
+                                ],
+                            )
+                        ],
+                        md=8,
+                    ),
+                ]
+            )
+        ]
+    ),
+]
+
+TOP_BANKS_PLOT = [
+    dbc.CardHeader(html.H5("Top 10 banks by number of complaints")),
+    dbc.CardBody(
+        [
+            dcc.Loading(
+                id="loading-banks-hist",
+                children=[
+                    dbc.Alert(
+                        "Not enough data to render this plot, please adjust the filters",
+                        id="no-data-alert-bank",
+                        color="warning",
+                        style={"display": "none"},
+                    ),
+                    dcc.Graph(id="bank-sample"),
+                ],
+                type="default",
+            )
+        ],
+        style={"marginTop": 0, "marginBottom": 0},
+    ),
+]
+
+TOP_BIGRAM_PLOT = [
+    dbc.CardHeader(html.H5("Top bigrams found in the database")),
+    dbc.CardBody(
+        [
+            dcc.Loading(
+                id="loading-bigrams-scatter",
+                children=[
+                    dbc.Alert(
+                        "Something's gone wrong! Give us a moment, but try loading this page again if problem persists.",
+                        id="no-data-alert-bigrams",
+                        color="warning",
+                        style={"display": "none"},
+                    ),
+                    dbc.Row(
+                        [
+                            dbc.Col(html.P(["Choose a t-SNE perplexity value:"]), md=6),
+                            dbc.Col(
+                                [
+                                    dcc.Dropdown(
+                                        id="bigrams-perplex-dropdown",
+                                        options=[
+                                            {"label": str(i), "value": i}
+                                            for i in range(3, 7)
+                                        ],
+                                        value=3,
+                                    )
+                                ],
+                                md=3,
+                            ),
+                        ]
+                    ),
+                    dcc.Graph(id="bigrams-scatter"),
+                ],
+                type="default",
+            )
+        ],
+        style={"marginTop": 0, "marginBottom": 0},
+    ),
+]
+
+TOP_BIGRAM_COMPS = [
+    dbc.CardHeader(html.H5("Comparison of bigrams for two companies")),
+    dbc.CardBody(
+        [
+            dcc.Loading(
+                id="loading-bigrams-comps",
+                children=[
+                    dbc.Alert(
+                        "Something's gone wrong! Give us a moment, but try loading this page again if problem persists.",
+                        id="no-data-alert-bigrams_comp",
+                        color="warning",
+                        style={"display": "none"},
+                    ),
+                    dbc.Row(
+                        [
+                            dbc.Col(html.P("Choose two companies to compare:"), md=12),
+                            dbc.Col(
+                                [
+                                    dcc.Dropdown(
+                                        id="bigrams-comp_1",
+                                        options=[
+                                            {"label": i, "value": i}
+                                            for i in lst_utilities
+                                        ],
+                                        value="EQUIFAX, INC.",
+                                    )
+                                ],
+                                md=6,
+                            ),
+                            dbc.Col(
+                                [
+                                    dcc.Dropdown(
+                                        id="bigrams-comp_2",
+                                        options=[
+                                            {"label": i, "value": i}
+                                            for i in lst_baa
+                                        ],
+                                        value="TRANSUNION INTERMEDIATE HOLDINGS, INC.",
+                                    )
+                                ],
+                                md=6,
+                            ),
+                        ]
+                    ),
+                    dcc.Graph(id="bigrams-comps"),
+                ],
+                type="default",
+            )
+        ],
+        style={"marginTop": 0, "marginBottom": 0},
+    ),
+]
+
+BODY = dbc.Container(
+    [
+        dbc.Row([dbc.Col(dbc.Card(TOP_BIGRAM_COMPS)),], style={"marginTop": 30}),
+        dbc.Row([dbc.Col(dbc.Card(TOP_BIGRAM_PLOT)),], style={"marginTop": 30}),
+        dbc.Row(
+            [
+                dbc.Col(LEFT_COLUMN, md=4, align="center"),
+                dbc.Col(dbc.Card(TOP_BANKS_PLOT), md=8),
+            ],
+            style={"marginTop": 30},
+        ),
+        dbc.Card(WORDCLOUD_PLOTS),
+        dbc.Row([dbc.Col([dbc.Card(LDA_PLOTS)])], style={"marginTop": 50}),
+    ],
+    className="mt-12",
+)
+
+
+
+app.layout = html.Div(children=[NAVBAR, BODY])
 if __name__ == '__main__':
     app.run_server(host='127.0.0.1', port='8050', debug=True)
