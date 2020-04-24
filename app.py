d6f4cfd959afac00903ca591cfec3cc7379a5ce5
tonydu97 <dutony97@gmail.com>
2020-04-23 22:50:36 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,37 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div(children=[
+    html.Header(children = [
+    html.H1(children='Charles River Associates')
+    ], style = {'background-color': 'red'}),
+    
+    html.H2('NextEra M&A - Project Bulldog'),
+
+    html.Div(children='''
+        Tony Du's python dashboard project
+    '''),
+
+
+
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(host='127.0.0.1', port='8050', debug=True)
